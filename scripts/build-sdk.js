54885f48a56f1fbea44b643c10928781bd0c26a0
bmwecker <141860318+bmwecker@users.noreply.github.com>
2025-02-27 03:12:40 +0100

    Исправлены проблемы совместимости

--- scripts/build-sdk.js
+++ scripts/build-sdk.js
@@ -6,9 +6,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 build({
-  entryPoints: [join(__dirname, '../public/sdk-bundle.js')],
+  entryPoints: [join(__dirname, '../src/sdk-bundle.js')],
   bundle: true,
   outfile: join(__dirname, '../public/sdk.min.js'),
   format: 'esm',
   minify: true,
+  platform: 'browser',
+  target: ['es2020'],
+  external: ['node:*'],
+  loader: {
+    '.js': 'jsx'
+  }
 }).catch(() => process.exit(1)); 
